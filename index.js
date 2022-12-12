8aa0dbf69a5f49822fd4fb73d572c42c2285977b
ANUJGARG120 <garganuj470@gmail.com>
2022-12-12 13:00:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function bookingMe(){
+    alert("Please Wait....")
+    confirm('Would you like to book your appointment')
+    setTimeout(()=>{
+        
+         alert('The Consultation appointment meeting has been Booked!!!')
+        },2000)
+
+}
+function clickMe(){
+setTimeout(()=>{
+alert('The Consultation appointment meeting has been confirmed!!!')
+},0)
+}
+
+
